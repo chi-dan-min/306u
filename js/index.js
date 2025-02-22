e4b993cf128100486e6515586b795b4aba0a2a08
ChiDan <chiidan2k4@gmail.com>
2025-02-23 01:46:20 +0700

    update

--- js/index.js
+++ js/index.js
@@ -1,15 +1,16 @@
 const title = document.querySelector('.title')
 const text = 'I Have Something'.split('')
+
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
-    title.innerHTML += `<span>${text[index]}<span/>`
+    title.innerHTML += `<span>${text[index]}</span>`
   } else {
-    title.innerHTML += `<span style='margin-right: 20px;'><span/>`
+    title.innerHTML += `<span style='margin-right: 20px;'></span>`
   }
 }
 
 const textElements = document.querySelectorAll('.title span');
 textElements.forEach((element) => {
-  const randomDelay = Math.random() * 3; // Menghasilkan delay acak antara 0 hingga 3 detik
+  const randomDelay = Math.random() * 3; // Tạo độ trễ ngẫu nhiên từ 0 đến 3 giây
   element.style.animationDelay = `${randomDelay}s`;
 });
